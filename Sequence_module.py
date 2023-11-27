c4b88ca29541c0a8d186c6493a55560dd85c70c0
charithasumeet <112922037+charithasumeet@users.noreply.github.com>
2023-11-27 17:18:59 -0500

    Add files via upload

---
+++ Sequence_module.py
@@ -0,0 +1,16 @@
+##########SEQUENCE MODULE
+
+class Sequence:
+
+    def __init__(self, id='', desc='', sequ='', leng=0, pbsq=0, gc=0, gcp=0):
+        # Initialize the variables
+        self.id = id
+        self.desc = desc
+        self.seq = sequ
+        self.length = leng
+        self.pbsq = pbsq
+        self.gc = gc
+        self.gcpercent = gcp
+
+
+
